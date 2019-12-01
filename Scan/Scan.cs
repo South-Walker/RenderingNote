4a35442c5709a656f5aa8a022199204ad4c0eabe
South-Walker <506830915@qq.com>
2019-12-01 22:12:02 +0800

    scan

---
+++ Scan/Scan.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scan : MonoBehaviour
+{
+    public Shader postshader;
+    public Camera cam;
+    private Material postmat;
+    public float radius;
+    public float width = 5;
+    public RenderTexture rt;
+    public Vector3 centerpos = new Vector3(0, 0, 0);
+    public float fade = 5;
+    private float totaltime = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam.depthTextureMode |= DepthTextureMode.Depth;
+        postmat = new Material(postshader);
+        rt = new RenderTexture(Screen.width, Screen.height, 16);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        totaltime += Time.deltaTime;
+    }
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Matrix4x4 frustumCorners = Matrix4x4.identity;
+
+        float fovWHalf = cam.fieldOfView * 0.5f;
+
+        Vector3 toRight = cam.transform.right * cam.nearClipPlane * Mathf.Tan(fovWHalf * Mathf.Deg2Rad) * cam.aspect;
+        Vector3 toTop = cam.transform.up * cam.nearClipPlane * Mathf.Tan(fovWHalf * Mathf.Deg2Rad);
+
+        Vector3 topLeft = (cam.transform.forward * cam.nearClipPlane - toRight + toTop);
+        float camScale = topLeft.magnitude * cam.farClipPlane / cam.nearClipPlane;
+
+        topLeft.Normalize();
+        topLeft *= camScale;
+
+        Vector3 topRight = (cam.transform.forward * cam.nearClipPlane + toRight + toTop);
+        topRight.Normalize();
+        topRight *= camScale;
+
+        Vector3 bottomRight = (cam.transform.forward * cam.nearClipPlane + toRight - toTop);
+        bottomRight.Normalize();
+        bottomRight *= camScale;
+
+        Vector3 bottomLeft = (cam.transform.forward * cam.nearClipPlane - toRight - toTop);
+        bottomLeft.Normalize();
+        bottomLeft *= camScale;
+
+        frustumCorners.SetRow(0, topLeft);
+        frustumCorners.SetRow(1, topRight);
+        frustumCorners.SetRow(2, bottomRight);
+        frustumCorners.SetRow(3, bottomLeft);
+        postmat.SetMatrix("_FrustumCorners", frustumCorners);
+        postmat.SetVector("_CentPos", centerpos);
+        postmat.SetFloat("_Radius", radius);
+        postmat.SetFloat("_Width", width);
+        postmat.SetFloat("_Fade", fade);
+        CustomGraphicsBlit(source, rt, postmat);
+
+        //postmat.SetTexture("_MainTex", source);
+        postmat.SetTexture("_MaskTex", rt);
+        Graphics.Blit(source, destination, postmat, 1);
+    }
+    private static void CustomGraphicsBlit(RenderTexture source, RenderTexture dest, Material fxMaterial)
+    {
+        RenderTexture.active = dest;
+        GL.PushMatrix();
+        GL.LoadOrtho();
+
+        fxMaterial.SetPass(0);
+
+        GL.Begin(GL.QUADS);
+
+        GL.MultiTexCoord2(0, 0.0f, 0.0f);
+        GL.Vertex3(0.0f, 0.0f, 3.0f); // BL
+
+        GL.MultiTexCoord2(0, 1.0f, 0.0f);
+        GL.Vertex3(1.0f, 0.0f, 2.0f); // BR
+
+        GL.MultiTexCoord2(0, 1.0f, 1.0f);
+        GL.Vertex3(1.0f, 1.0f, 1.0f); // TR
+
+        GL.MultiTexCoord2(0, 0.0f, 1.0f);
+        GL.Vertex3(0.0f, 1.0f, 0.0f); // TL
+
+        GL.End();
+        GL.PopMatrix();
+    }
+}
---
+++ Scan/Scan.shader
@@ -0,0 +1,107 @@
+﻿Shader "Hidden/Scan"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+    SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+				float3 interpolatedRay : TEXCOORD1;
+                float4 vertex : SV_POSITION;
+            };
+			sampler2D_float _CameraDepthTexture;
+			float4x4 _FrustumCorners;
+			float _Radius;
+			float _Width;
+			float _Fade;
+			float3 _CentPos;
+
+			v2f vert(appdata v)
+			{
+				v2f o;
+				half index = v.vertex.z;
+				v.vertex.z = 0.1;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv.xy;
+				o.interpolatedRay = _FrustumCorners[(int)index].xyz;
+				return o;
+			}
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+				fixed depth = Linear01Depth(SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture, UnityStereoTransformScreenSpaceTex(i.uv)));
+
+				fixed4 worldPos = fixed4(depth * i.interpolatedRay, 1);
+
+				worldPos.xyz += _WorldSpaceCameraPos;
+
+				fixed dis = length(_CentPos.xyz - worldPos.xyz);
+				//fixed pre = saturate(dis - _Radius + abs(dis - _Radius));
+				//pre /= 2;
+				fixed a = 1 - saturate((abs(dis - _Radius) - _Width) / _Fade);
+				return fixed4(a, a, a, a);
+            }
+            ENDCG
+        }
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float4 vertex : SV_POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			sampler2D _MainTex;
+			sampler2D _MaskTex;
+			v2f vert(appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+
+			fixed4 frag(v2f i) : SV_Target
+			{
+				fixed4 col = tex2D(_MainTex, i.uv);
+				fixed4 mask = tex2D(_MaskTex, i.uv) * 0.75;
+				fixed4 maskcol = fixed4(0.75, 0.75, 0, 1);
+				col += mask * maskcol;
+				return col;
+			}
+				ENDCG
+		}
+	}
+}
